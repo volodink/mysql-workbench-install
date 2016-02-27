4e6c2669dd68caa797e82847f9a075bc6bee2b50
Konstantin Volodin <volodin.konstantin@gmail.com>
2016-02-27 13:52:07 +0300

    Create install_mysql.sh

---
+++ install_mysql.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget ttp://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-6.3.6-1ubu1404-amd64.deb
+
+dpkg -i mysql*.deb
+apt-get -f install -y
+dpkg -i mysql*.deb
+
+rm mysql*.deb
